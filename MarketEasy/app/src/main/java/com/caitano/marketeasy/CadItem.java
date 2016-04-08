89ca5664e07cc759c86a19813694bbec3eea638d
Caitano Junior <caitanojunior@gmail.com>
2016-04-07 22:54:31 -0300

    create main screen

---
+++ MarketEasy/app/src/main/java/com/caitano/marketeasy/CadItem.java
@@ -0,0 +1,16 @@
+package com.caitano.marketeasy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by caitano on 07/04/16.
+ */
+public class CadItem  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cad_item);
+    }
+}
--- MarketEasy/app/src/main/java/com/caitano/marketeasy/MainActivity.java
+++ MarketEasy/app/src/main/java/com/caitano/marketeasy/MainActivity.java
@@ -1,5 +1,7 @@
 package com.caitano.marketeasy;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -40,5 +45,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startCadItem(View view) {
+
+        Intent cad_item = new Intent(this, CadItem.class);
+        startActivity(cad_item);
     }
 }
